5ffc275ff9c768f79cae5dc55625dcecd00da4b8
Leo Soriano <soriano.leo98@gmail.com>
2024-10-04 15:14:21 +0200

    Version Ôtsutsuki

--- js/ajoutbd.js
+++ js/ajoutbd.js
@@ -13,7 +13,116 @@ boutons.forEach(bouton => { // Boucle sur chaque bouton
     });
   });
 
-  /* Fonction d'ajout d'une nouvelle BD
+
+let texte = localStorage.getItem("panierBD");
+let panierBD = JSON.parse(texte);
+let i = 0;
+
+
+let panierBDi = JSON.parse(localStorage.getItem("panierBD"))
+
+const container = document.getElementById("carte-container");
+
+// Clear the existing content of the container
+container.innerHTML = ''; 
+
+
+for (const item of panierBD) {
+  let album = albums.get(item.numero);
+
+  if (album) {
+    let divAlbum = document.createElement("div");
+    divAlbum.classList.add("carte"); // Ajoute la classe "carte" pour le style
+
+    let serie = series.get(album.idSerie);
+    let auteur = auteurs.get(album.idAuteur);
+    let imgBD = serie.nom + "-" + album.numero + "-" + album.titre + ".jpg";
+
+    // Image
+    let imgElement = document.createElement("img");
+    imgElement.src = `albumsMini/${serie.nom.replace(/'|!|\?|\.|"|:|\$/g, "")}-${album.numero}-${album.titre.replace(/'|!|\?|\.|"|:|\$/g, "")}.jpg`;
+    imgElement.classList.add("panierImg");
+    imgElement.alt = album.titre;
+
+    // Croix pour supprimer la carte
+    let closeButton = document.createElement("span");
+    closeButton.classList.add("close-button");
+    closeButton.textContent = "X"; // Utilisez un "X" pour la croix
+    closeButton.dataset.index = i; // Stocker l'index dans un attribut de données
+
+    // Titre
+    let titleElement = document.createElement("h3");
+    titleElement.textContent = album.titre + " x" + panierBD[i].quantité;
+
+    // Prix
+    let prixElement = document.createElement("h5");
+    prixElement.textContent = "Sous-total: " + album.prix * panierBD[i].quantité + "€";
+
+    // Fonction pour supprimer la carte et l'élément du panierBD
+    closeButton.addEventListener("click", () => {
+      // Supprimer la carte de la page
+      //divAlbum.remove();
+
+      // Récupérer l'index à partir de l'attribut de données
+      const index = parseInt(closeButton.dataset.index, 10);
+
+      // Supprimer l'album du panierBD
+      panierBD.splice(index, 1);
+      location.reload();
+      // Mettre à jour le localStorage
+      localStorage.setItem("panierBD", JSON.stringify(panierBD));
+    });
+
+    // Ajout des éléments à la carte
+    divAlbum.appendChild(closeButton);
+    divAlbum.appendChild(imgElement);
+    divAlbum.appendChild(titleElement);
+    divAlbum.appendChild(prixElement);
+   
+
+    i++;
+
+    // Ajout de la carte au container
+    container.appendChild(divAlbum);
+  }
+}
+
+  
+   
+
+
+
+
+
+
+
+
+
+
+function supprimerBDpanier(button) {
+    let index = button.dataset.index; 
+    let panier = localStorage.getItem("panierBD");
+    panier = JSON.parse(panier);
+    panier.splice(index, 1);
+    localStorage.setItem("panierBD", JSON.stringify(panier));
+    location.reload(); 
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     /* Fonction d'ajout d'une nouvelle BD
 *
 *
 */
@@ -24,12 +133,12 @@ function ajouterBD(key) {
     // Verifie si la key existe déjà dans le panierBD
     let index = panierBD.findIndex(item => item.numero === key);
 
-    if (index !== -1) { // Si la clé existe déjà
+    if (index !== -1) { // Si la key existe déjà
         panierBD[index].quantité++; // Incrémenter la quantité
     } else { // Si la key n'existe pas
         panierBD.push({ numero: key, quantité: 1 }); // Ajouter la key avec une quantité de 1
     }
-
+    location.reload();
     // Met a jour la liste dans le localStorage
     localStorage.setItem("panierBD", JSON.stringify(panierBD));
 }
